3e785f6557a625a0d6fc23d5ef44ed8134423f66
Armando Luhut <armandoluhutsidabutar@students.polmed.ac.id>
2022-08-23 14:50:54 +0700

    perbaharuan

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,24 @@ Route::delete('/books/{id}', 'BookController@destroy');
 
 Route::get('/books/edit/{id}', 'BookController@edit');
 
-Route::get('/books', function(Request $request){
-    return "Respon ini nantinya akan digunakan untuk mengambil semua data buku";
-});
+// Route::get('/books', function(Request $request){
+//     return "Respon ini nantinya akan digunakan untuk mengambil semua data buku";
+// });
 
-Route::post('/books', function(){
-    return "Router ini nantinya akan digunakan untuk menciptakan data buku yang baru";
-});
+// Route::post('/books', function(){
+//     return "Router ini nantinya akan digunakan untuk menciptakan data buku yang baru";
+// });
 
-Route::get('/books/{id}', function($id){
-    return "Respon ini nantinya akan digunakan untuk mengambil satu data buku dengan id=" .$id;
-});
+// Route::get('/books/{id}', function($id){
+//     return "Respon ini nantinya akan digunakan untuk mengambil satu data buku dengan id=" .$id;
+// });
 
-Route::put('/books/{id}', function($id){
-    return "Router ini nantinya akan digunakan untuk mengubah data buku dengan id=" .$id;
-});
+// Route::put('/books/{id}', function($id){
+//     return "Router ini nantinya akan digunakan untuk mengubah data buku dengan id=" .$id;
+// });
  
-Route::delete('/books/id}', function($id){
-    return "Router ini nantinya digunakan untuk menghapus data buku dengan id=" .$id;
-});
+// Route::delete('/books/id}', function($id){
+//     return "Router ini nantinya digunakan untuk menghapus data buku dengan id=" .$id;
+// });
 
 ?>
